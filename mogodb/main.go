d816fe4b36c880121effe57b2192c084e9c8d90a
gou yu <2250257231@qq.com>
2021-04-19 08:49:33 +0800

    go的mangodb操作

--- mogodb/main.go
+++ mogodb/main.go
@@ -14,7 +14,7 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
-		"mongodb+srv://root:<asdf>@ssk.3hxej.mongodb.net/GOAPIPROJECT?retryWrites=true&w=majority",
+		"mongodb+srv://root:asdf@ssk.3hxej.mongodb.net/GOAPIPROJDB?retryWrites=true&w=majority&authSource=admin",
 	))
 	if err != nil {
 		log.Fatal(err)
