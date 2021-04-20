f3b670cf9488c13168001412c87be74a076f386c
gou yu <2250257231@qq.com>
2021-04-20 15:33:34 +0800

    大鸟展玮

--- mongodb/main.go
+++ mongodb/main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+//连接数据库的语句
 var uri string = "mongodb+srv://root:asdf@ssk.3hxej.mongodb.net/GOAPIPROJDB?retryWrites=true&w=majority&authSource=admin"
 
-type User struct {
-	Name string
-	Age  int
-}
-
 //连接数据库操作
+// uri:连接语句
+// DBName:操作的数据库名称
+// timeout:设置超时时间
+// numOfConnection:最大链接数量
+// collectionName:集合的名字
+// 返回:*mongo.Collection操作的集合对象
 func connectToMongoDB(uri string, DBName string, timeout time.Duration, numOfConnection uint64, collectionName string) (*mongo.Collection, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
@@ -33,7 +33,12 @@ func connectToMongoDB(uri string, DBName string, timeout time.Duration, numOfCon
 	return client.Database(DBName).Collection(collectionName), nil
 }
 
-//C操作
+// @title    Create
+// @description   插入操作
+// @auth      Laozhu
+// @param    collection        *mongo.Collection         "操作的Collection"
+// @param    obj               []interface{}             "要插入的东西，可以是一个，可以是多个"
+// @return    InsertedIDs      []interface{}             "插入的对象的ID，不为nil表明插入成功"
 func Create(collection *mongo.Collection, obj []interface{}) []interface{} {
 	insertResult, err := collection.InsertMany(context.TODO(), obj)
 	if err != nil {
@@ -43,10 +48,15 @@ func Create(collection *mongo.Collection, obj []interface{}) []interface{} {
 	return insertResult.InsertedIDs
 }
 
-//R操作
-func Read(collection *mongo.Collection /*, obj []interface{}*/) []interface{} {
+// @title    Read
+// @description   查找操作
+// @auth      Laozhu
+// @param    collection        *mongo.Collection         "操作的Collection"
+// @param    fliter              interface{}             "筛选器，筛选出要更新的文档"
+// @return    results      []interface{}                 "查询结果返回值为空或者nil代表没查到和错误"
+func Read(collection *mongo.Collection, fliter interface{}) []interface{} {
 
-	cur, err := collection.Find(context.TODO(), bson.D{{}})
+	cur, err := collection.Find(context.TODO(), fliter)
 	if err != nil {
 		log.Fatal(err)
 		return nil
@@ -70,47 +80,49 @@ func Read(collection *mongo.Collection /*, obj []interface{}*/) []interface{} {
 	return results
 }
 
-//U操作
-func Update(collection *mongo.Collection, fliter, obj *interface{}) *mongo.UpdateResult {
+// @title    Update
+// @description   更新操作
+// @auth      Laozhu
+// @param    collection        *mongo.Collection         "操作的Collection"
+// @param    fliter            interface{}               "筛选器，筛选出要更新的文档"
+// @param    obj               interface{}               "更新规则，怎么操作更新"
+// @return   updateResult      *mongo.UpdateResult       "更新的结果，如果失败则返回nil"
+func Update(collection *mongo.Collection, fliter, obj interface{}) *mongo.UpdateResult {
 
 	//log.Fatal("not implemented Error")
 	updateResult, err := collection.UpdateMany(context.TODO(), fliter, obj)
 	if err != nil {
 		log.Fatal(err)
+		return nil
 	}
 	return updateResult
 }
 
-//D操作
+// @title    Delete
+// @description   删除操作
+// @auth      Laozhu
+// @param    collection        *mongo.Collection         "操作的Collection"
+// @param    fliter            *interface{}              "筛选器，筛选出要更新的文档"
+// @return   deleteResult      *mongo.DelteResult        "删除的结果，如果失败则返回nil"
 func Delete(collection *mongo.Collection, fliter interface{}) *mongo.DeleteResult {
 	deleteResult, err := collection.DeleteMany(context.TODO(), fliter)
 	if err != nil {
 		log.Fatal(err)
+		return nil
 	}
 	return deleteResult
 }
 
+//删库跑路
+func 删库跑路(collection *mongo.Collection) {
+	collection.DeleteMany(context.TODO(), bson.D{{}})
+}
+
 func main() {
-	lr := LogRecord{
-		jobName: "append",
-		command: "-0t",
-		err:     "no",
-		content: "shabi",
-		tp: TimePoint{
-			startTime: 1,
-			endTime:   2,
-		},
-	}
-	var iResult *mongo.InsertOneResult
-	var err error
 	collection, _ := connectToMongoDB(uri, "GOAPIPROJDB", 10*time.Second, 5, "test")
-	if iResult, err = collection.InsertOne(context.TODO(), &lr); err != nil {
-		fmt.Print(err)
-		return
-	}
-	//_id:默认生成一个全局唯一ID
-	id := iResult.InsertedID.(primitive.ObjectID)
-	fmt.Println("自增ID", id.Hex())
+	删库跑路(collection)
+	result := Read(collection, bson.D{{}})
+	print(result)
 }
 
 //
--- mongodb/model.go
+++ mongodb/model.go
@@ -1,14 +1,8 @@
 package main
 
-type TimePoint struct {
-	startTime int64 `bson:"startTime"`
-	endTime   int64 `bson:"endTime"`
-}
-
-type LogRecord struct {
-	jobName string `bson:"jobName"`
-	command string `bson:"command"`
-	err     string `bson:"err"`
-	content string `bson:"content"`
-	tp      TimePoint
+type Student struct {
+	ID     int64
+	Name   string
+	Gender string
+	Class  int
 }
